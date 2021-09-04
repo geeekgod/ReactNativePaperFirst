1099f558c5b75a4f89e36e00c27274c9ae15be4f
Rishabh Singh <ris8645208@gmail.com>
2021-09-04 16:55:19 +0530

    feat: add cards to home

--- src/screens/App/App.js
+++ src/screens/App/App.js
@@ -1,8 +1,17 @@
 import { useNavigation } from "@react-navigation/core";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Button, useTheme } from "react-native-paper";
-import AppBar from "../../components/AppBar";
+import { useEffect } from "react";
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  useTheme,
+  Avatar,
+  Card,
+  Title,
+  Paragraph,
+} from "react-native-paper";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
   const styles = StyleSheet.create({
@@ -13,13 +22,63 @@ export default function App() {
     },
   });
 
+  const [post, setPost] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      let response = await fetch("https://jsonplaceholder.typicode.com/posts");
+      let json = await response.json();
+      setPost(json);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const renderPost = ( {item} ) =>(
+        <Card style={{padding:10, margin:15}}>
+          <Card.Title
+            title={item.title}
+            // left={LeftContent}
+          />
+          <Card.Content>
+            <Paragraph>{item.body}</Paragraph>
+          </Card.Content>
+          <Card.Actions>
+            <Button>Cancel</Button>
+            <Button>Ok</Button>
+          </Card.Actions>
+        </Card>
+  )
+
   const navigation = useNavigation();
-  return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <Button mode="contained" onPress={() => navigation.navigate("Details")}>
-        Go to details
-      </Button>
-    </View>
-  );
+  const [postsLoaded, setPostsLoaded] = useState(false);
+  if (postsLoaded) {
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+        <Button mode="contained" onPress={() => navigation.navigate("Details")}>
+          Go to details
+        </Button>
+        {post && (
+          <FlatList
+            data={post}
+            renderItem={renderPost}
+            keyExtractor={(item) => item.id}
+          />
+        )}
+      </View>
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getPosts}
+        onFinish={() => setPostsLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
 }
