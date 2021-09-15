10e88b771fe8fa74df4ed8f966ce1a380cc6d6a0
Rishabh Singh <ris8645208@gmail.com>
2021-09-15 12:00:00 +0530

    style: customize paper cards

--- src/screens/PostDetails/PostDetails.js
+++ src/screens/PostDetails/PostDetails.js
@@ -1,5 +1,6 @@
 import { useNavigation } from "@react-navigation/core";
 import * as React from "react";
+import { View } from "react-native";
 import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
 
 const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
@@ -8,24 +9,60 @@ const PostDetails = ({ route }) => {
   const item = route.params;
   const navigation = useNavigation();
   return (
-    <Card style={{ padding: 10, margin: 15 }}>
-      <Card.Cover
-        source={{ uri: "https://picsum.photos/seed/" + item.id + "/700" }}
-      />
-      <Card.Title title={item.title} />
-      <Card.Content>
-        <Paragraph>{item.body}</Paragraph>
-      </Card.Content>
-      <Card.Actions>
-        <Button
-          icon="home"
-          style={{ marginTop: 40 }}
-          mode="contained"
-          onPress={() => navigation.popToTop()}
+    <Card
+      style={{
+        padding: 10,
+        margin: 15,
+        borderWidth: 0,
+        maxHeight: 290,
+        elevation: 0,
+        borderRadius: 20,
+      }}
+    >
+      <View style={{ display: "flex", flexDirection: "row" }}>
+        <View
+          style={{
+            width: "40%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+          }}
         >
-          Go Back to Feed
-        </Button>
-      </Card.Actions>
+          <Card.Cover
+            style={{
+              height: "97%",
+              width: "100%",
+              borderTopLeftRadius: 20,
+              borderBottomLeftRadius: 20,
+              marginVertical: "auto",
+            }}
+            source={{ uri: "https://picsum.photos/seed/" + item.id + "/700" }}
+          />
+        </View>
+        <View
+          style={{
+            width: "60%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Card.Title title={item.title} numberOfLines={3} />
+          <Card.Content>
+            <Paragraph numberOfLines={7}>{item.body}</Paragraph>
+          </Card.Content>
+          <Card.Actions>
+            <Button
+              icon="home"
+              style={{ marginTop: 10 }}
+              mode="contained"
+              onPress={() => navigation.popToTop()}
+            >
+              Go Back to Feed
+            </Button>
+          </Card.Actions>
+        </View>
+      </View>
     </Card>
   );
 };
