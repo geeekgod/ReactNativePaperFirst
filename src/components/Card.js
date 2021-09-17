d19d2641389f42f5022c288a414f17c6ada99fab
Rishabh Singh <ris8645208@gmail.com>
2021-09-17 13:29:48 +0530

    chore: update post data

--- src/components/Card.js
+++ src/components/Card.js
@@ -32,7 +32,11 @@ const CustomCard = ({ item, pageType }) => {
             style={{
               marginTop: 10,
               borderRadius: 8,
-              elevation: 0,
+              elevation: 2,
+              width: 120,
+              borderRadius: 32,
+              shadowOpacity: 5,
+              backgroundColor: "rgba(193, 12, 153, 1)",
             }}
             mode="contained"
             onPress={() => navigation.popToTop()}
@@ -48,7 +52,7 @@ const CustomCard = ({ item, pageType }) => {
         padding: 10,
         margin: 15,
         borderWidth: 0,
-        maxHeight: 290,
+        maxHeight: 250,
         maxWidth: Dimensions.get("screen").width - 25,
         elevation: 0,
         borderRadius: 20,
@@ -85,7 +89,11 @@ const CustomCard = ({ item, pageType }) => {
         >
           <Card.Title title={item.title} numberOfLines={3} />
           <Card.Content>
-            <Paragraph numberOfLines={7}>{item.body}</Paragraph>
+            {/* <Paragraph numberOfLines={7}>{item.body}</Paragraph> */}
+            <Paragraph>Servings: {item.userId}</Paragraph>
+            <Paragraph>
+              Time: {item.userId * Math.floor(Math.random() * 40)} Mins
+            </Paragraph>
           </Card.Content>
           <Card.Actions>{buttonOnType()}</Card.Actions>
         </View>
